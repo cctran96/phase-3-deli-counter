62652a319f551365ae2398f852a99c10d85ea9a3
Chau Tran <cctran96@gmail.com>
2021-05-16 19:06:36 -0700

    commit tree 47ad6e79ad3e43c9d2cdb3b2a513e7d3f6ad7c00

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+def line(line)
+    def big_line(line)
+        arr = []
+        line.each.with_index(1){|name, index| arr << "#{index}. #{name}"}
+        "The line is currently: #{arr.join(' ')}"
+    end
+    puts line.size == 0 ? "The line is currently empty." : big_line(line)
+end
+
+def take_a_number(line, name)
+    line << name
+    puts "Welcome, #{name}. You are number #{line.size} in line."
+end
+
+def now_serving(line)
+    def serve(line)
+        message = "Currently serving #{line.first}."
+        line.shift
+        message
+    end
+    puts line.size == 0 ? "There is nobody waiting to be served!" : serve(line)
+end
